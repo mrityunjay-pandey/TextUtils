eb6adc23cf70ec06843a79b1763830d04348fa59
mrityunjay-pandey <140483827+mrityunjay-pandey@users.noreply.github.com>
2023-08-29 15:00:43 +0530

    Video number 11 -Exercise 1 solution

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About';
+import TextForm from './components/TextForm';
+// import About from './components/About';
 function App() {
   return (
 <>
 <Navbar title = "TextUtils"/>
 <div className="container my-3 ">
-{/* <TextForm heading = "Enter the text to analyze below"/> */}
-<About/>
+<TextForm heading = "Enter the text to analyze below"/>
+{/* <About/> */}
 </div>
 </>  );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,6 +17,23 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
     
+    const handleClearClick = ()=>{
+        // console.log("clear text")
+        let newText = '';
+        setText(newText)
+    }
+    const handleCopy =() =>{
+        console.log("I am a copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+        let newText =text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+    
     const [text, setText] = useState('');
     // text = "new text"; //wrong way to change the state
     // setText = "new text"; //correct way to change the state
@@ -28,8 +45,11 @@ export default function TextForm(props) {
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick ={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick ={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-1" onClick ={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-1" onClick ={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick ={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-1" onClick ={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-1" onClick ={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
     <div className = "containe my-3">
         <h2>Your text summary</h2>
