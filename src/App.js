a3adc83373c3b9f697b22f2eb77f9ff5b8830da0
mrityunjay-pandey <140483827+mrityunjay-pandey@users.noreply.github.com>
2023-08-29 09:13:53 +0530

    video number 4 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "Mrityunjay";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Mrityunjay
-        </a>
-      </header>
-    </div>
-  );
+<>
+<nav>
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+</nav>
+<div className="container">
+  <h1>Hello {name}</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem corporis nobis aspernatur quod nisi molestias, ducimus saepe? Vel odio aut possimus autem alias porro cum et labore sint perferendis vero iusto vitae, a laudantium?</p>
+</div>
+</>  );
 }
 
 export default App;
